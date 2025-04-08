0ea9691b2bd56786ed0b9a4585590366e762342a
offGabrieel <gabriellou23@hotmail.com>
2025-04-08 16:12:50 +0000

    Estruturas de repetições das peças adicionada

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,39 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    int bispo = 1;
+    int torre = 1;
+    int rainha = 1;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    
+    while (bispo <= 5){
+        
+        printf("Bispo: Cima, Direita.\n");
+        
+        bispo++;
+    }
+        printf("\n");
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    
+    do {
+        
+        printf("Torre: Direita.\n");
+        torre++;
+        
+    } while (torre <= 5);
+        printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    
+    for (rainha = 1; rainha <= 5; rainha++) {
+        printf("Rainha: Esquerda.\n");
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
