249bc86deedfd0cc4c73bd23a5b71d2e430a2ca1
offGabrieel <gabriellou23@hotmail.com>
2025-04-10 15:08:55 +0000

    Adicionando Recursividade nos movimentos

--- xadrez.c
+++ xadrez.c
@@ -4,13 +4,34 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void moverTorre(int casasTorre){
+    if (casasTorre > 0){
+        printf("Direita\n");
+            moverTorre(casasTorre - 1);
+    }
+}
+    
+void moverBispo(int casasBispo){
+    if (casasBispo > 0){
+        printf("Direita, Cima\n");
+            moverBispo(casasBispo - 1);
+    }
+}
+void moverRainha(int casasRainha){
+    if (casasRainha > 0){
+        printf("Esquerda\n");
+            moverRainha(casasRainha - 1);
+    }
+}
+
+
+
+
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
     
-    int bispo = 1;
-    int torre = 1;
-    int rainha = 1;
     int cavalo = 1;
     int opcao;
     
@@ -29,35 +50,19 @@ int main() {
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
     
-    while (bispo <= 5){
-        
-        printf("Bispo: Cima, Direita.\n");
-        
-        bispo++;
-    }
-        printf("\n");
+    moverBispo(5);
     break;
     case 2:
-
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     
-    do {
-        
-        printf("Torre: Direita.\n");
-        torre++;
-        
-    } while (torre <= 5);
-        printf("\n");
+    moverTorre(5);
+    
     break;
     case 3:
-
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
     
-    for (rainha = 1; rainha <= 5; rainha++) {
-        printf("Rainha: Esquerda.\n");
-    }
+    moverRainha(8);
     break;
 
     // Nível Aventureiro - Movimentação do Cavalo
