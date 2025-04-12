9b850cfcb7a26b1fa856b38199d7700a69c812c7
offGabrieel <gabriellou23@hotmail.com>
2025-04-12 03:38:35 +0000

    Opção de direções adicionada no movimento das peças

--- xadrez.c
+++ xadrez.c
@@ -6,25 +6,89 @@
 
 // Implementa funções recursivas para simular o movimento de cada peça
 
-void moverTorre(int casasTorre){
-    if (casasTorre > 0){
-        printf("Direita\n");
-        moverTorre(casasTorre - 1);
-    }
-}
-    
-void moverBispo(int casasBispo){
-    while (casasBispo--){
-        for (int i = 0; i < 1; i++){
-            printf("Direita, ");
+// Função recursiva responsalvel pelos movimentos da Bispo.
+
+void bispo(int direcao, int casas){
+    if(casas > 0){
+        bispo(direcao, casas-1);
+        switch (direcao){
+            case 1:
+                printf("Cima, ");
+                printf("Direita\n");
+                break;
+            case 2:
+                printf("Cima, ");
+                printf("Esquerda\n");
+                break;
+            case 3:
+                printf("Baixo, ");
+                printf("Direita\n");
+                break;
+            case 4:
+                printf("Baixo, ");
+                printf("Esquerda\n");
+                break;
         }
-        printf("Cima\n");
     }
 }
-void moverRainha(int casasRainha){
-    if (casasRainha > 0){
-        printf("Esquerda\n");
-        moverRainha(casasRainha - 1);
+
+// Função recursiva responsalvel pelos movimentos da torre.
+
+void torre(int direcao, int casas){
+    if(casas > 0){
+        torre(direcao, casas-1);
+        switch(direcao){
+            case 1:
+                printf("Esquerda\n");
+                break;
+            case 2:
+                printf("Direita\n");
+                break;
+            case 3:
+                printf("Cima\n");
+                break;
+            case 4:
+                printf("Baixo\n");
+                break;
+        }
+    }
+} 
+
+// Função recursiva responsalvel pelos movimentos do cavalo.
+
+void rainha(int direcao, int casas){
+    if(casas > 0){
+        rainha(direcao, casas-1);
+        switch(direcao){
+            case 1:
+                printf("Direita\n");
+                break;
+            case 2:
+                printf("Esquerda\n");
+                break;
+            case 3:
+                printf("Cima\n");
+                break;
+            case 4:
+                printf("Baixo\n");
+                break;
+            case 5:
+                printf("Cima, ");
+                printf("Dirita\n");
+                break;
+            case 6:
+                printf("Cima, ");
+                printf("Esquerda\n");
+                break;
+            case 7:
+                printf("Baixo, ");
+                printf("Direita\n");
+                break;
+            case 8:
+                printf("Baixo, ");
+                printf("Esquerda\n");
+                break;
+        }
     }
 }
 
@@ -32,10 +96,9 @@ void moverRainha(int casasRainha){
 
 int main(){
     
-    int movimentoBispo, movimentoTorre, movimentoRainha, movimentoCavalo, opcao;
-    char finalizar;
+    int movimentoCavalo, opcao, direcao, casas, finalizar;
     
-    // Menu de interação com as peças
+    // implementação do menu de interação com as peças.
     
     do{  
         printf("Escolha qual peça deseja movimentar!\n");
@@ -49,53 +112,82 @@ int main(){
         switch (opcao){
             case 1:
     
-            // Implementação de Movimentação do Bispo
+            // Implementação de Movimentação do Bispo.
         
             do{
-                printf("Quantas casas deseja mover? (max.5)\n");
-                scanf("%d", &movimentoBispo);
-                
-                if(movimentoBispo < 1 || movimentoBispo > 5){
-                    printf("Quantidade de casas inválida! (max.5) \n");
-                }
-            }while(movimentoBispo < 1 || movimentoBispo > 5);
-        
-            moverBispo(movimentoBispo);
+                    printf("Qual direção desejar mover o Bispo? \n");
+                    printf("1. Cima, Direita.\n");
+                    printf("2. Cima, Esquerda.\n");
+                    printf("3. Baixo, Direita.\n");
+                    printf("4. Baixo, Esquerda.\n");
+                    scanf("%d", &direcao);
+                    
+                    printf("Quantas casas deseja mover o Bispo? (Max. 5)\n");
+                    scanf("%d", &casas);
+                    printf("\n");
     
-            break;
-            case 2:
-        
-            // Implementação de Movimentação da Torre
-        
-            do{
-                printf("Quantas casas deseja mover? (max.5)\n");
-                scanf("%d", &movimentoTorre);
+                    if(casas < 1 || casas > 5){
+                        printf("Quantidade de casas inválida! (max.5) \n");
+                    }
+                }while (casas < 1 || casas > 5);
+            
+            bispo(direcao, casas);
                 
-                if(movimentoTorre < 1 || movimentoTorre > 5){
-                    printf("Quantidade de casas inválida! (max.5) \n");
-                }
-            }while(movimentoTorre < 1 || movimentoTorre > 5);
-        
-            moverTorre(movimentoTorre);
-
-            break;
+                break;
+            case 2:
+            
+            // Implementação de Movimentação da Torre.
+            
+                do{
+                    printf("Qual direção desejar mover a Torre? \n");
+                    printf("1. Direita.\n");
+                    printf("2. Esquerda.\n");
+                    printf("3. Cima.\n");
+                    printf("4. Baixo.\n");
+                    scanf("%d", &direcao);
+                    
+                    printf("Quantas casas deseja mover a Torre? (Max. 5)\n");
+                    scanf("%d", &casas);
+                    printf("\n");
+    
+                    if(casas < 1 || casas > 5){
+                        printf("Quantidade de casas inválida! (max.5) \n");
+                    }
+                }while (casas < 1 || casas > 5);
+            
+            torre(direcao, casas);
+                
+                break;
             case 3:
             
-            // Implementação de Movimentação da Rainha
+            // Implementação de Movimentação da Rainha.
     
             do{
-                printf("Quantas casas deseja mover? (max.5)\n");
-                scanf("%d", &movimentoRainha);
-                
-                if(movimentoRainha < 1 || movimentoRainha > 8){
-                    printf("Quantidade de casas inválida! (max.8) \n");
-                }
-            }while(movimentoRainha < 1 || movimentoRainha > 8);
-        
-            moverRainha(movimentoRainha);
-            break;
+                    printf("Qual direção desejar mover a Rainha? \n");
+                    printf("1. Direita.\n");
+                    printf("2. Esquerda.\n");
+                    printf("3. Cima.\n");
+                    printf("4. Baixo.\n");
+                    printf("5. Cima, Direita.\n");
+                    printf("6. Cima, Esquerda.\n");
+                    printf("7. Baixo, Direita\n");
+                    printf("8. Baixo, Esquerda.\n");
+                    scanf("%d", &direcao);
+                    
+                    printf("Quantas casas deseja mover a Rainha? (Max. 8)\n");
+                    scanf("%d", &casas);
+                    printf("\n");
+    
+                    if(casas < 1 || casas > 8){
+                        printf("Quantidade de casas inválida! (max.8) \n");
+                    }
+                }while (casas < 1 || casas > 8);
             
-            // Implementação de Movimentação do Cavalo
+            rainha(direcao, casas);
+                
+                break;
+            
+            // Implementação de Movimentação do Cavalo ("Apenas o movimento inicial do cavalo").
 
             case 4:
             do{
@@ -130,12 +222,14 @@ int main(){
                 break;
     } 
         
-        // Implementação de sistema de repetição do jogo
+        // Implementação de sistema de repetição/finalização do jogo.
         
-        printf("Continuar jogando?(S/N)\n");
-        scanf(" %c", &finalizar);
+        printf("Continuar jogando?\n");
+        printf("1. SIM.\n");
+        printf("2. NÃO.\n");
+        scanf(" %d", &finalizar);
     
-        }while (finalizar == 's' || finalizar == 'S');
+        }while (finalizar == 1 );
     
         printf("Jogo encerrado!");
     
